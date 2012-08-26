4ed6a7876718032635c3d997f4eb589500d23d8a
Michael Yagudaev <michael@yagudaev.com>
2012-08-25 22:08:39 -0500

    front page created. Also made a modification to the footer

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,17 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Winnipeg.js' });
+  res.render('index', { title: 'Winnipeg.js', page: 'index' });
 };
 
 exports.events = function(req, res){
-  res.render('events', { title: 'Events' });
+  res.render('events', { title: 'Events', page: 'events'});
 };
 
 exports.resources = function(req, res){
-  res.render('resources', { title: 'Resources' });
+  res.render('resources', { title: 'Resources', page: 'resources' });
 };
 
 exports.members = function(req, res){
-  res.render('members', { title: 'Members' });
+  res.render('members', { title: 'Members', page: 'members'});
 };
