98043d8b42fd84ec7bd8a15a5434f7c5e5b07b32
Jacob-Friesen <F.Jacob.J@gmail.com>
2012-12-19 22:22:15 -0600

    cookie based mobile to desktop interface changing

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
+// file wide constants
+var EVENT_PAGE_NAME = 'event-page';
+var EVENT_PRE_TITLE = "Event ";
+
 var fs = require('fs'),
 	path = require('path');
 
@@ -27,7 +31,7 @@ exports.eventPage = function(req, res) {
 	
 	fs.exists(path.normalize(__dirname + '/../views/' + page + '.jade'), function(exists) {
 		if (exists) {
-			res.render(page, { title: 'Event ' + req.params.date, page: 'event-page', to_desktop: to_desktop(req)});	
+			res.render(page, { title: EVENT_PRE_TITLE + req.params.date, page: EVENT_PAGE_NAME, to_desktop: to_desktop(req)});	
 		} else {
 			res.render('404', { title: 'Page not Found 404', page: '404', to_desktop: to_desktop(req)});
 		}
@@ -35,6 +39,33 @@ exports.eventPage = function(req, res) {
 	
 }
 
+// Sets a cookie in the users browser specifying they or don't want the desktop interface (for mobile only).
+// Not going to call any of the functions above as that would rely on none of them changing names later.
+exports.set_desktop = function(req, res) {
+  if (req.query.page !== {}){
+	if (req.query.page === '') {
+	  req.query.page = 'index';
+	}
+	var page = req.query.page;
+	
+	// event pages are in a subdirectory so the path must be modified. Since the user is already on the page
+	// the file system does not need to check for the pages existence.
+	if (page === EVENT_PAGE_NAME) {
+	  page = 'events/' + req.query.title.replace(EVENT_PRE_TITLE, '');
+	}
+	
+	// set to_desktop cookie so subsequent pages know whether to render a desktop version for mobile or not
+	res.setHeader('Set-Cookie','test=value');
+	res.cookie('desktop_interface', req.query.to + '', {maxAge: 86400000});// day in milleseconds
+	
+	res.render(page, { title: req.query.title, page: req.query.page, to_desktop: req.query.to == 'true' ? true : false});
+  }
+  else {
+	res.render('404', { title: 'Page not Found 404', page: '404', to_desktop: req.query.to == 'true' ? true : false});
+  }
+}
+
+// Checks if a desktop interface is requested (via a cookie)
 function to_desktop(req){
-  return req.query.to_desktop == '' ? '?to_desktop' : '';
+  return req.cookies.desktop_interface == 'true' ? true : false;
 }
